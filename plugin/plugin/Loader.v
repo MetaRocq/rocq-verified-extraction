3183ae7f9b551b7621f64d495e9b81fda0a8ac14
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2024-07-22 17:59:45 +0200

    Ensure that mapping of bool is done by extractions and update README to document Extract Inductive command

--- plugin/plugin/Loader.v
+++ plugin/plugin/Loader.v
@@ -5,3 +5,6 @@ From Malfunction Require Export PrintMli.
 Declare ML Module "coq_verified_extraction.plugin".
 Declare ML Module "coq-verified-extraction-ocaml.plugin".
 
+(** This is required for linking with the OCaml FFIs which assume
+    the ocaml ordering on [bool] values. *)
+Verified Extract Inductives [ bool => "bool" [ 1 0 ] ].
