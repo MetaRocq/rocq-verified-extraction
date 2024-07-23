22b5cae13eba1ed5fffd3a58f6087723af120da8
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2024-07-23 09:47:13 +0200

    Fix test-suite

--- examples/simple/test_extract_inductive.v
+++ examples/simple/test_extract_inductive.v
@@ -11,19 +11,15 @@ Verified Extract Inductives [
 Definition coq_true := true.
 Definition coq_false := false.
 (* Set Debug "verified-extraction". *)
-Verified Extraction -time -verbose coq_true.
-Verified Extraction -unsafe coq_true.
+Verified Extraction coq_true.
 Verified Extraction coq_false.
-Verified Extraction -unsafe coq_false.
-
 Verified Extraction andb.
-Verified Extraction -unsafe andb.
 
 Verified Extract Inductives [
   option => "option" [ 1 0 ] (* This makes switches look at none cases before some *)
 ].
 
 Definition test_get := (@option_get nat 0%nat None).
-Verified Extraction -unsafe test_get.
+Verified Extraction test_get.
 
 
