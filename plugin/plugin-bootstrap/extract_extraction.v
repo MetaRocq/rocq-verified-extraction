e90f439e7aa36208c91e4d42aa1c6defc8959d0d
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2025-04-14 17:17:51 +0200

    Stop relying on opam environment for bootstrapping

--- plugin/plugin-bootstrap/extract_extraction.v
+++ plugin/plugin-bootstrap/extract_extraction.v
@@ -3,6 +3,8 @@ From Malfunction Require Import Pipeline.
 
 Set Verified Extraction Build Directory "_build".
 
+Unset Verified Extraction Use Opam.
+
 (* Malfunction.Plugin.Extract binds all primitives to OCaml externals *)
 Set Warnings "-primitive-turned-into-axiom".
 
